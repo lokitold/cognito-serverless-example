11fa8264068152fe22db69c5d72a0f528e347cc5
Victor Rojas Centeno <vico.16c@gmail.com>
2019-09-09 16:44:18 -0500

    cambio

--- handler.js
+++ handler.js
@@ -8,6 +8,7 @@ const request = require('request');
 const jwkToPem = require('jwk-to-pem');
 const jwt = require('jsonwebtoken');
 global.fetch = require('node-fetch');
+const cognitoidentityserviceprovider = new AWS.CognitoIdentityServiceProvider();
 //
 
 //const
@@ -144,4 +145,86 @@ module.exports.RefreshToken= (event, context, cb) => {
           //console.log(retObj);
       }
   })
+}
+
+
+// Accept a POST with a JSON structure containing the
+// refresh token provided during the original user login, 
+// and an old and new password.
+module.exports.changeUserPassword = (event, context, callback) => {
+  // Extract relevant JSON into a request dict (This is my own utility code)
+  let requiredFields = ['old_password','new_password','refresh_token'];
+  //let request = Utils.extractJSON(event['body'], requiredFields);
+  //if (request == false) {
+    //Utils.errorResponse("Invalid JSON or missing required fields", context.awsRequestId, callback);
+    //return; // Abort here
+    //}
+  let request = event['body'];
+
+
+  // This function can NOT be handled by admin APIs, so we need to
+  // authenticate the user (not the admin) and use that
+  // authentication instead.
+  let refreshToken = request['refresh_token']
+
+  // Authenticate as the user first, so we can call user version
+  // of the ChangePassword API
+  console.log(event);
+  cognitoidentityserviceprovider.adminInitiateAuth({
+    AuthFlow: 'REFRESH_TOKEN',
+    ClientId: poolData.ClientId,
+    UserPoolId: poolData.UserPoolId,
+    AuthParameters: {
+      'REFRESH_TOKEN': refreshToken
+    },
+    ContextData:  {
+      HttpHeaders: [ /* required */
+        {
+          headerName: 'User-Agent',
+          headerValue: event.headers['User-Agent']
+        },
+        /* more items */
+      ],
+      IpAddress: event.identity.sourceIp, /* required */
+      ServerName: event.headers['Host'], /* required */
+      ServerPath: '/api/update-password', /* required */
+      //EncodedData: 'STRING_VALUE'
+    }
+  }, function(err, data) {
+    if(err){
+      //Utils.errorResponse(err['message'], context.awsRequestId, callback);
+      callback(JSON.stringify(err));
+      
+      return // Abort here
+    } else {
+      // Now authenticated as user, change the password
+      let accessToken = data['AuthenticationResult']['AccessToken'] // Returned from auth - diff signature than Authorization header
+      let oldPass = request['old_password']
+      let newPass = request['new_password']
+      let params = {
+        AccessToken: accessToken, /* required */
+        PreviousPassword: oldPass, /* required */
+        ProposedPassword: newPass /* required */
+      }
+
+      // At this point, really just a pass through
+      cognitoidentityserviceprovider.changePassword(params, function(err2, data2) {
+        if(err2){
+          let message = {
+            err_message: err2['message'],
+            access_token: accessToken
+          }
+          //Utils.errorResponse(message, context.awsRequestId, callback);
+          callback(JSON.stringify(message));
+        } else {
+          let response = {
+            'success': 'OK',
+            'response_data': data2 // Always seems to be empty
+          }
+          callback(null,response)
+        }
+      });
+    }
+  });
+
 }
